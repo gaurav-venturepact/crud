473d52f3736ab23757d0ed70e49840e8a83ff306
gaurav-venturepact <gaurav.kumar@venturepact.com>
2018-05-06 23:08:08 +0530

    router

--- crud/src/app/app.module.ts
+++ crud/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { HomeComponent } from './home/home.component';
 import { UserDataService } from './user-data.service';
 import { LoginComponent } from './login/login.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { RegisterComponent } from './register/register.component';
 
 
 @NgModule({
@@ -18,7 +19,8 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
     AppComponent,
     HomeComponent,
     LoginComponent,
-    NavBarComponent
+    NavBarComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
