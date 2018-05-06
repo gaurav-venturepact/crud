618228649bde8c14aaa5ee8c2c0e5b99cdf38904
gaurav-venturepact <gaurav.kumar@venturepact.com>
2018-05-06 22:51:10 +0530

    login form

--- crud/src/app/app-routing.module.ts
+++ crud/src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { LoginComponent } from './login/login.component';
+
+const routes: Routes = [
+  {path:'', component: LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+export const routingComponent = [LoginComponent];
--- crud/src/app/app.module.ts
+++ crud/src/app/app.module.ts
@@ -9,12 +9,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { UserDataService } from './user-data.service';
+import { LoginComponent } from './login/login.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
